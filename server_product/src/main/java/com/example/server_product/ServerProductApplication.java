06d1eb10be558af6179250b275daeb9b600aaf23
조용진 <101401582+Brio-yj@users.noreply.github.com>
2025-09-28 11:01:56 +0900

    >

---
+++ server_product/src/main/java/com/example/server_product/ServerProductApplication.java
@@ -0,0 +1,13 @@
+package com.example.server_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerProductApplication.class, args);
+    }
+
+}
---
+++ server_product/src/test/java/com/example/server_product/ServerProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
